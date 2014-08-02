351ebdd248dd4f30b5e58d65e424d92d23ad67c2
Whymarrh Whitby <whymarrh@users.noreply.github.com>
2014-08-02 19:03:11 -0230

    Use consistent quotes

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -4,7 +4,7 @@ file { '/var/www/':
 }
 
 # make sure that locale is set
-class { "locales": 
+class { 'locales':
 	locales   => ['en_CA.UTF-8 UTF-8'],
 }
 
@@ -15,10 +15,10 @@ package { ['sass']:
 
 # git install plus checkout the aba project
 include git
-vcsrepo { "/vagrant/ABALookup":
+vcsrepo { '/vagrant/ABALookup':
 	ensure   => latest,
 	provider => git,
-	source   => "https://github.com/MUNComputerScienceSociety/ABALookup.git",
+	source   => 'https://github.com/MUNComputerScienceSociety/ABALookup.git',
 }
 
 # node.js
