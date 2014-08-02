9791c9879cf00b822e6dbe7729584dcc696e5bd3
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2014-08-02 01:56:45 -0230

    Add Node.js, Bower, and Grunt JS

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -15,3 +15,14 @@ vcsrepo { "/vagrant/abaproject":
 	provider => git,
 	source   => "https://github.com/MUNComputerScienceSociety/ABALookup.git",
 }
+
+# node.js
+class { 'nodejs':
+	version      => 'stable',
+	make_install => false,
+}
+
+package { ['grunt-cli', 'bower']:
+	provider => npm,
+	require => Class['nodejs']
+}
