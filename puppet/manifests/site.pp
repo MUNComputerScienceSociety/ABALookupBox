5ec3d497bb6c929d0a5d3bd4d5a421ef2b544d10
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2014-08-02 02:26:32 -0230

    Add Sass

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -8,6 +8,11 @@ class { "locales":
 	locales   => ['en_CA.UTF-8 UTF-8'],
 }
 
+package { ['sass']:
+	ensure => 'installed',
+	provider => 'gem',
+}
+
 # git install plus checkout the aba project
 include git
 vcsrepo { "/vagrant/ABALookup":
